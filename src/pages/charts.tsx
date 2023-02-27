58f9dcf36624ae54f1348826819e4f05b961552c
nolanbruner <nolansname@aol.com>
2023-02-27 14:32:10 -0500

    added trading view

---
+++ src/pages/charts.tsx
@@ -0,0 +1,78 @@
+// TradingViewWidget.jsx
+
+import React, { useEffect, useRef } from 'react';
+import * as tv from "react-ts-tradingview-widgets"
+export const Chart1 =()=>{
+return(
+    <>
+    <tv.AdvancedRealTimeChart symbol='BTC'/>
+    
+    </>
+)
+
+}
+
+// let tvScriptLoadingPromise: Promise<unknown>;
+
+// export default function TradingViewWidget() {
+//   const onLoadScriptRef = useRef();
+
+//   useEffect(
+//     () => {
+//       onLoadScriptRef.current = createWidget;
+
+//       if (!tvScriptLoadingPromise) {
+//         tvScriptLoadingPromise = new Promise((resolve) => {
+//           const script = document.createElement('script');
+//           script.id = 'tradingview-widget-loading-script';
+//           script.src = 'https://s3.tradingview.com/tv.js';
+//           script.type = 'text/javascript';
+//           script.onload = resolve;
+
+//           document.head.appendChild(script);
+//         });
+//       }
+
+//       tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+//       return () => onLoadScriptRef.current ;
+
+//       function createWidget() {
+//         if (document.getElementById('tradingview_2f503') && 'TradingView' in window) {
+//           new window.TradingView.widget({
+//             autosize: true,
+//             symbol: "NASDAQ:AAPL",
+//             interval: "D",
+//             timezone: "Etc/UTC",
+//             theme: "light",
+//             style: "1",
+//             locale: "en",
+//             toolbar_bg: "#f1f3f6",
+//             enable_publishing: false,
+//             allow_symbol_change: true,
+//             container_id: "tradingview_2f503"
+//           });
+//         }
+//       }
+//     },
+//     []
+//   );
+
+//   return (
+//     <div className='tradingview-widget-container'>
+//       <div id='tradingview_2f503' />
+//       <div className="tradingview-widget-copyright">
+//         <a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span className="blue-text">AAPL stock chart</span></a> by TradingView
+//       </div>
+//     </div>
+//   );
+// }
+// // import React from "react";
+
+// // export const Chart1 = ()=>{
+
+// //     return(
+// //        <div color="white"> Chart1
+// //        </div>
+// //         )
+// // }

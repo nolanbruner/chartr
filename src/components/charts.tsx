6f5f273f7142c089cae6b2db98af48272ed3e923
nolanbruner <nolansname@aol.com>
2023-02-27 15:51:38 -0500

    removed chart

--- src/components/charts.tsx
+++ src/components/charts.tsx
@@ -2,12 +2,15 @@
 
 import React from 'react';
 import * as tv from "react-ts-tradingview-widgets"
-export const Chart1 =()=>{
-return(
-    <>
-    <tv.AdvancedRealTimeChart symbol='BTC'/>
-    
-    </>
-)
+
+export const Chart1 = () => {
+    return (
+        <>
+            <div> <tv.SingleTicker /></div>
+            {/* <div>  <tv.AdvancedRealTimeChart /></div> */}
+            <div>  <tv.CompanyProfile /></div>
+            <div> <tv.MarketData /></div>
+        </>
+    )
 
 }
